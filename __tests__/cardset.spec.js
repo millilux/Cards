59276728a5d53e3bec45b97b4046025e418630a6
onefourtwoeightfiveseven <brian@briananderson.co.uk>
2016-04-19 00:49:17 +0100

    Add babel + refactor
    - Make CardSet an iterable
    - append/prepend accept multiple arguments

--- __tests__/cardset.spec.js
+++ __tests__/cardset.spec.js
@@ -4,11 +4,19 @@ const cards = require('../lib/cards');
 const Suit = cards.Suit;
 const Rank = cards.Rank;
 
-
 describe('append', () => {
-    it('has 1 card when appended', () => {
-        var cardSet = new cards.CardSet();
-        cardSet.append(new cards.Card(Suit.ACE, Rank.SPADES));
+    it('can append 1 card', () => {
+        let cardSet = new cards.CardSet();
+        let deck = new cards.Deck();
+        cardSet.append(deck.pop());
         expect(cardSet.count()).toBe(1);
+    });
+
+    it('can append multiple cards', () => {
+        let cardSet = new cards.CardSet();
+        let deck = new cards.Deck();
+        let i = 5;
+        while (i--) cardSet.append(deck.pop());
+        expect(cardSet.count()).toBe(5);
     });
 });
