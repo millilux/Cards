4ea81d0f63be902c9a104dc58f22a900a04158c2
onefourtwoeightfiveseven <brian@briananderson.co.uk>
2016-04-18 17:15:13 +0100

    Add a couple more tests

--- math.js
+++ math.js
@@ -2,20 +2,20 @@
 
 module.exports = {
 
-	randomInt : function (min, max){
-    	return Math.floor(Math.random()*(max-min+1)+min);
-	},
+    randomInt : function (min, max){
+        return Math.floor(Math.random()*(max-min+1)+min);
+    },
 
-	lerp : function (a, b, t) {
-    	return (1-t)*a + t*b;
-	},
+    lerp : function (a, b, t) {
+        return (1-t)*a + t*b;
+    },
 
-	map : function(value, inputMin, inputMax, outputMin, outputMax){
-    	return ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
-	},
+    map : function(value, inputMin, inputMax, outputMin, outputMax){
+        return ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
+    },
 
-	clamp : function(value, min, max){
-    	return Math.min(Math.max(value, min), max);
-	}
+    clamp : function(value, min, max){
+        return Math.min(Math.max(value, min), max);
+    }
 
 };
